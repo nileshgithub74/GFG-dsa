a62365d6cd5254b702f3f5371997fd2f2c35350b
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2025-01-30 16:13:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCases = Integer.parseInt(br.readLine());
+
+        while (testCases-- > 0) {
+            // Read capacity
+            int capacity = Integer.parseInt(br.readLine());
+
+            // Read values
+            String[] valInput = br.readLine().split(" ");
+            int[] val = new int[valInput.length];
+            for (int i = 0; i < valInput.length; i++) {
+                val[i] = Integer.parseInt(valInput[i]);
+            }
+
+            // Read weights
+            String[] wtInput = br.readLine().split(" ");
+            int[] wt = new int[wtInput.length];
+            for (int i = 0; i < wtInput.length; i++) {
+                wt[i] = Integer.parseInt(wtInput[i]);
+            }
+
+            // Call the knapSack function and print the result
+            System.out.println(Solution.knapSack(capacity, val, wt));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static int knapSack(int capacity, int val[], int wt[]) {
+    int n = val.length;
+        int[][] dp = new int[n + 1][capacity + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= capacity; j++) {
+                if (wt[i - 1] <= j) {
+                  
+                    dp[i][j] = Math.max(dp[i - 1][j], val[i - 1] + dp[i - 1][j - wt[i - 1]]);
+                } else 
+                {
+                    dp[i][j] = dp[i - 1][j]; 
+                }
+            }
+        }
+        return dp[n][capacity];
+    }
+}
