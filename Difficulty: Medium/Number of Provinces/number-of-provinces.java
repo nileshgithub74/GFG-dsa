cb876a5cfedace81be102c5cf17b6096136890b2
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2025-06-26 19:50:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Provinces/number-of-provinces.java
+++ Difficulty: Medium/Number of Provinces/number-of-provinces.java
@@ -1,22 +1,42 @@
+import java.util.ArrayList;
+
 class Solution {
-    static void dfs(ArrayList<ArrayList<Integer>> adj, int s, boolean[] visited, int V) {
+
+    static void dfs(ArrayList<ArrayList<Integer>> adjlist, int s, boolean[] visited) {
         visited[s] = true;
 
-        for (int j = 0; j < V; j++) {
-            if (adj.get(s).get(j) == 1 && !visited[j]) {
-                dfs(adj, j, visited, V);
+        for (int neighbor : adjlist.get(s)) {
+            if (!visited[neighbor]) {
+                dfs(adjlist, neighbor, visited);
             }
         }
     }
 
     static int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
+
+        ArrayList<ArrayList<Integer>> adjlist = new ArrayList<>();
+
+        // Convert adjacency matrix to adjacency list
+        for (int i = 0; i < V; i++) {
+            adjlist.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (adj.get(i).get(j) == 1 && i != j) {
+                    adjlist.get(i).add(j);
+                    adjlist.get(j).add(i);
+                }
+            }
+        }
+
         boolean[] visited = new boolean[V];
         int count = 0;
 
         for (int i = 0; i < V; i++) {
             if (!visited[i]) {
                 count++;
-                dfs(adj, i, visited, V);
+                dfs(adjlist, i, visited); 
             }
         }
 
