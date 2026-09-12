2424570a77b2762efb4b174e7c7a2672b5891cad
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2026-09-12 22:08:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Digits/reverse-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverseDigits(int n) {
+        // Code here
+        
+        int reversed =0;
+        int sum =0;
+        
+        while( n != 0){
+            int digit =  n %10;
+            reversed = reversed * 10 + digit;
+            n = n /10;
+        }
+        
+        return reversed;
+        
+    }
+}
